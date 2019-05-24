06c889123034579d80c9e6358691b695c0f348aa
Jean-Louis Huynen <huynenjl@gmail.com>
2019-05-24 16:42:20 +0200

    new: [passive-dns] initial script for building pdns demo VM

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,27 +19,60 @@ sudo apt-get -y upgrade > /dev/null 2>&1
 sudo apt-get -y autoremove > /dev/null 2>&1
 
 echo "--- Install base packages ---"
-sudo apt-get -y install git python sudo tmux vim virtualenvwrapper virtualenv zip python3-pythonmagick htop imagemagick asciidoctor jq ntp ntpdate > /dev/null 2>&1
-## Remove mailutils, it probably makes the script stuck on a user prompt....
+sudo apt-get -y install binutils-dev ldnsutils libldns-dev libpcap-dev libdate-simple-perl golang-go autoconf git python sudo tmux vim virtualenvwrapper virtualenv zip python3-pythonmagick htop imagemagick asciidoctor jq ntp ntpdate
+#sudo apt-get -y install git-core binutils-dev libldns1 libldns-dev libpcap-dev libdate-simple-perl golang-go autoconf git python sudo tmux vim virtualenvwrapper virtualenv zip python3-pythonmagick htop imagemagick asciidoctor jq ntp ntpdate > /dev/null 2>&1
 
 echo "--- Installing and configuring Postfix ---"
 # # Postfix Configuration: Satellite system
 # # change the relay server later with:
 # sudo postconf -e 'relayhost = example.com'
 # sudo postfix reload
-echo "postfix postfix/mailname string `hostname`.misp.local" | debconf-set-selections
-echo "postfix postfix/main_mailer_type string 'Satellite system'" | debconf-set-selections
+echo "postfix postfix/mailname string `hostname`.misp.local" | sudo debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Satellite system'" | sudo debconf-set-selections
 sudo apt-get install -y postfix > /dev/null 2>&1
 
 echo "--- Retrieving D4 ---"
-sudo -u d4 git clone https://github.com/D4-project/d4-core.git $PATH_TO_D4/d4-core
+git clone https://github.com/D4-project/d4-core.git $PATH_TO_D4/d4-core
 echo "--- Installing dependencies ---"
-cd $PATH_TO_D4/d4-core/server
-sudo -u d4 ./install_server.sh
+pushd $PATH_TO_D4/d4-core/server
+./install_server.sh
 echo "--- Creating Certificates  ---"
-cd gen_cert
-sudo -u d4 ./gen_root.sh
-sudo -u d4 ./gen_cert.sh
+pushd gen_cert
+./gen_root.sh
+./gen_cert.sh
+popd 
+popd 
+
+echo "--- Installing d4-goclient ---"
+go get github.com/satori/go.uuid
+go get github.com/D4-project/d4-goclient
+pushd $PATH_TO_D4/go/src/github.com/D4-project/d4-goclient
+make amd64l
+mkdir conf.vbox
+popd conf.vbox
+echo "127.0.0.1:4443" > destination
+echo "stdin" > source
+echo "private key to change" > key
+echo "1" > version
+echo "8" > type
+touch uuid
+popd
+
+echo "--- Retrieving PassiveDNS ---"
+ git clone https://github.com/gamelinux/passivedns $PATH_TO_D4/passivedns
+echo "--- Installing PassiveDNS ---"
+pushd $PATH_TO_D4/passivedns
+autoreconf --install 
+./configure
+make
+sudo make install
+popd
+echo "--- Retrieving analyzer-d4-passivedns ---"
+git clone https://github.com/D4-project/analyzer-d4-passivedns.git $PATH_TO_D4/analyzer-d4-passivedns
+pushd $PATH_TO_D4/analyzer-d4-passivedns
+./install_server.sh
+mkdir DB
+popd
 
 echo "--- Writing rc.local  ---"
 # With initd:
@@ -47,14 +80,16 @@ if [ ! -e /etc/rc.local ]
 then
     echo '#!/bin/sh -e' | sudo tee -a /etc/rc.local
     echo 'exit 0' | sudo tee -a /etc/rc.local
-    chmod u+x /etc/rc.local
+    sudo chmod u+x /etc/rc.local
 fi
 
 # redis-server requires the following /sys/kernel tweak
-sed -i -e '$i \echo never > /sys/kernel/mm/transparent_hugepage/enabled\n' /etc/rc.local
-sed -i -e '$i \echo 1024 > /proc/sys/net/core/somaxconn\n' /etc/rc.local
-sed -i -e '$i \sysctl vm.overcommit_memory=1\n' /etc/rc.local
-sed -i -e '$i \sudo -u d4 bash -c "(cd /home/d4/d4-core/server; ./LAUNCH.sh -l > /tmp/d4.log)"\n' /etc/rc.local
+sudo sed -i -e '$i \echo never > /sys/kernel/mm/transparent_hugepage/enabled\n' /etc/rc.local
+sudo sed -i -e '$i \echo 1024 > /proc/sys/net/core/somaxconn\n' /etc/rc.local
+sudo sed -i -e '$i \sysctl vm.overcommit_memory=1\n' /etc/rc.local
+sudo sed -i -e '$i \bash -c "(cd /home/d4/d4-core/server; ./LAUNCH.sh -l > /tmp/d4.log)"\n' /etc/rc.local
+sudo sed -i -e '$i \screen -dmS "passiveDNS" \n'  /etc/rc.local
+sudo sed -i -e '$i \screen -S "passiveDNS" -X screen -t "pdns-lookup" bash -c "(/home/d4/analyzer-d4-passivedns/redis/src/redis-server /home/d4/analyzer-d4-passivedns/etc/redis.conf & > /tmp/redis-pdns.log)" \n' /etc/rc.local 
 
 TIME_END=$(date +%s)
 TIME_DELTA=$(expr ${TIME_END} - ${TIME_START})
